3af6d524461489380631bc80e2906ea312b572fb
José Ulisses <julissesmacedo@outlook.com>
2025-03-24 14:30:00 -0300

    Adicionando modulo url-shortener ao app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UrlShortenerModule } from './url-shortener/url-shortener.module';
+import { AuthModule } from './auth/auth.module';
+import { UrlShorteningService } from './url-sortening/url-sortening.service';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { UrlShortenerService } from './url-shortener/url-shortener.service';
+import { urlShortenerProviders } from './url-shortener/url-shortener.providers';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    UrlShortenerService,
+    UrlShorteningService,
+    ...urlShortenerProviders,
+  ],
+  imports: [UrlShortenerModule, AuthModule, UsersModule, DatabaseModule],
 })
 export class AppModule {}
