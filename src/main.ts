f7cc02e854f6e40f48f527cc74ea9de0c0313bf1
José Ulisses <julissesmacedo@outlook.com>
2025-03-24 14:20:26 -0300

    Adicionando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Shortener example')
+    .setDescription('The shortener API description')
+    .setVersion('1.0')
+    .addTag('shortener')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
